818eecabf27e0f2dc4fbbbe4df37832813dde979
Diego Veiga <diego.gveiga@yahoo.com>
2021-06-24 14:47:36 -0300

    #

---
+++ src/components/Button.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { 
+    Text, 
+    TouchableOpacity, 
+    StyleSheet, 
+    TouchableOpacityProps 
+} from "react-native";
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+interface ButtonProps extends TouchableOpacityProps {
+    title: String
+}
+
+export function Button({ title, ... rest }: ButtonProps) {
+    return(
+        <TouchableOpacity 
+            style={styles.container}
+            activeOpacity={0.7}
+            {... rest}
+        >
+            <Text style={styles.text}>
+                { title }
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        height: 56,
+        paddingHorizontal: 10
+    },
+
+    text: {
+        fontSize: 16,
+        color: colors.white,
+        fontFamily: fonts.heading
+    }
+})
